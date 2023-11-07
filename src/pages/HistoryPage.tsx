d1e93e12ecdccd605629faec37a4808fb76d4d04
VeejaLiu <veejaliu@gmail.com>
2023-11-07 17:26:22 +0800

    修改历史页面样式

--- src/pages/HistoryPage.tsx
+++ src/pages/HistoryPage.tsx
@@ -18,16 +18,34 @@ export const styleMap: string[] = [
     "Trash polka",
 ];
 
+const statusMap: string[] = [
+    "Processing",
+    "Success",
+    "Fail",
+];
+
 function HistoryPage() {
+
+    // {
+    // 	"id": 130,
+    // 	"userId": "admin",
+    // 	"style": 1,
+    // 	"prompt": "一只可爱的猫",
+    // 	"promptEnglish": "A cute cat",
+    // 	"generateUsedTime": 3000,
+    // 	"status": 1,
+    // 	"images": [
+    // 		"http://123.60.97.192:9001/pic/2023-11-07T17:04:13.829028_1.png",
+    // 		"http://123.60.97.192:9001/pic/2023-11-07T17:04:13.829028_1.png",
+    // 		"http://123.60.97.192:9001/pic/2023-11-07T17:04:13.829028_1.png",
+    // 		"http://123.60.97.192:9001/pic/2023-11-07T17:04:13.829028_1.png"
+    // 	],
+    // 	"createTime": "2023-10-19T11:19:04.000Z"
+    // },
     const [history, setHistory] = useState<any[]>([]);
 
     const getHistory = async () => {
         const result = await HistoryApi.getHistory();
-        console.log(result);
-        result.history.forEach((item: any) => {
-                item.images = JSON.parse(item.images);
-            }
-        );
         console.log(result);
         setHistory(result.history);
     }
@@ -64,7 +82,13 @@ function HistoryPage() {
                         </>
                     }
                 >
-                    {`item.prompt_history_id: ${item.prompt_history_id}`}
+                    <p><b>ID:</b> {item.id} </p>
+                    <p><b>Style:</b> {styleMap[item.style]} </p>
+                    <p><b>Prompt:</b> {item.prompt} </p>
+                    <p><b>Prompt English:</b> {item.promptEnglish} </p>
+                    <p><b>Generate Used Time:</b> {item.generateUsedTime} </p>
+                    <p><b>Status:</b> {statusMap[item.status]} </p>
+                    <p><b>Create Time:</b> {item.createTime} </p>
                 </List.Item>
             )
             }
